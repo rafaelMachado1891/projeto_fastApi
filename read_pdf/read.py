4d563beeb4b0b96a3973d85148a3d2a29b654ee8
rafaelMachado1891 <rafadmn1@gmail.com>
2025-05-20 23:06:23 -0300

    atualizando leitura da última pagina

--- read_pdf/read.py
+++ read_pdf/read.py
@@ -14,20 +14,32 @@ path = (f'..\data\{file_name}.pdf')
 tables = camelot.read_pdf(
     path,
     flavor="stream",
-    table_areas=["12, 759,549,241"],
+    table_areas=["12, 759,549,31"],
     #columns=["70, 105, 160, 230, 285, 340, 380, 446"],
     #strip_text=".\n",
-    pages="5-end",
+    pages="1-4",
 )
 
-print(tables[0].parsing_report)
+pages_end = camelot.read_pdf(
+    path,
+    flavor="stream",
+    table_areas=["12, 759,549,224"],
+    pages="5"
+)
+
+# print(tables[0].parsing_report)
 
 camelot.plot(tables[0], kind="contour")
 
 plt.show()
 
-print(tables[0].df)
+# print(tables[0].df)
 
-#df_total = pd.concat([table.df for table in tables], ignore_index=True)
+df_total = pd.concat(
+    [table.df for table in tables] + [table.df for table in pages_end],
+    ignore_index=True
+)
 
-#print(df_total)
+print(df_total)
+
+df_total.to_csv("dados.csv", index=False)
