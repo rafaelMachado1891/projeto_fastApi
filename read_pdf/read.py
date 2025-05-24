4adf19c2eb57c9053a0e15e79a35b8fb33930e53
rafaelMachado1891 <rafadmn1@gmail.com>
2025-05-24 17:36:33 -0300

    pre tratamento dos dados e carga para o banco

--- read_pdf/read.py
+++ read_pdf/read.py
@@ -1,9 +1,24 @@
 import os
-
 import camelot
 import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv()
+
+database = os.getenv("DB_NAME_PROD")
+username = os.getenv("DB_USER_PROD")
+host = os.getenv("DB_HOST_PROD")
+password = os.getenv("DB_PASS_PROD")
+port = os.getenv("DB_PORT_PROD")
+
+# URL de conexão com o banco de dados
+DATABASE_URL = (f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}")
+
+engine = create_engine(DATABASE_URL)
 
 print(matplotlib.get_backend())
 
@@ -31,7 +46,7 @@ pages_end = camelot.read_pdf(
 
 camelot.plot(tables[0], kind="contour")
 
-plt.show()
+# plt.show()
 
 # print(tables[0].df)
 
@@ -40,6 +55,44 @@ df_total = pd.concat(
     ignore_index=True
 )
 
-print(df_total)
+df = df_total
 
-df_total.to_csv("dados.csv", index=False)
+df.columns = df.iloc[0]
+
+df = df[df['Reduzido'] != 'Reduzido']
+
+def tratar_coluna_saldo(saldo):
+    i= saldo.replace(".","").replace(",",".")
+    i=i.split(")")
+    i=i[0]
+    i=i.replace("(", "-")
+    return i 
+
+def tratar_coluna_conta(conta):
+    i=conta.replace("(-)", "")
+    i=i.strip()
+    return i
+
+df["Saldo atual"] = df["Saldo atual"].apply(tratar_coluna_saldo)
+df["Nome da conta"] = df["Nome da conta"].apply(tratar_coluna_conta)
+
+df = df.rename(columns={"Reduzido": "id_conta",
+                        "Nome da conta": "conta",
+                        "Saldo atual": "saldo"
+                        })
+
+tipo_de_dados = {
+    "id_conta": int,
+    "conta": str,
+    "saldo": float
+}
+
+df=df.astype(tipo_de_dados)
+
+df.to_sql(
+    name="balancete",
+    schema=os.getenv('DB_SCHEMA_PROD'),  # Schema do banco de destino
+    if_exists='replace',          # Adicionar dados à tabela existente,
+    con=engine,
+    index=False                  # Não incluir o índice do DataFrame
+    )
